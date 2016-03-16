015042d85643c7d940d1dcb29251d4b87a061a38
BobGerman <bobg@bluemetal.com>
2016-03-15 21:54:54 -0400

    Added comment about this to the Methods sample

--- VS2013/LearnTypeScript/2-Classes/02-Methods.ts
+++ VS2013/LearnTypeScript/2-Classes/02-Methods.ts
@@ -1,5 +1,13 @@
 (() : void => {
 
+    // Quick review of the this keyword:
+    //
+    // 1 - If you use apply, bind, or call, you specify this
+    // 2 - If you invoke the function in the form someObject.myFunc(),
+    //     this is someObject
+    // 3 - If in strict mode, this will be undefined
+    // 4 - Otherwise this will be the global object
+
     class Person {
         
         private firstName: string;
